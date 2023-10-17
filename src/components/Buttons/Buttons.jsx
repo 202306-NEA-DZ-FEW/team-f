dbf6a2e862d4453d4d948669ab1df994417f8212
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-10-17 09:51:00 +0100

    feat(buttons.jsx): add uk flag as a button
    Buttons #34

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import styles from "@/styles/buttons.module.css";
 import { AiFillCaretDown } from "react-icons/ai";
 import { BsFilterRight } from "react-icons/bs";
+import { BiSolidUpArrowCircle, BiSolidShareAlt } from "react-icons/bi";
+import { HiMiniUserCircle } from "react-icons/hi2";
+import { MdEdit } from "react-icons/md";
+import { GoTrash } from "react-icons/go";
+import { GrFacebook, GrInstagram, GrLinkedin } from "react-icons/gr";
+import Image from "next/image";
 
 export default function Buttons({
     donateNowBtn,
@@ -11,6 +17,16 @@ export default function Buttons({
     locationBtn,
     recentBtn,
     addItemBtn,
+    backToTop,
+    user,
+    editIcon,
+    trashIcon,
+    facebookIcon,
+    instagramIcon,
+    linkedinIcon,
+    shareIcon,
+    readMoreBtn,
+    ukFlag,
 }) {
     return (
         <div>
@@ -51,6 +67,62 @@ export default function Buttons({
             {addItemBtn && (
                 <button className={styles.addItemButton} onClick={addItemBtn}>
                     Add item
+                </button>
+            )}
+            {backToTop && (
+                <button onClick={backToTop}>
+                    <BiSolidUpArrowCircle className={styles.upArrowIcon} />
+                </button>
+            )}
+            {user && (
+                <button onClick={user}>
+                    <HiMiniUserCircle className={styles.user} />
+                </button>
+            )}
+            {editIcon && (
+                <button onClick={editIcon}>
+                    <MdEdit className={styles.editIcon} />
+                </button>
+            )}
+            {trashIcon && (
+                <button onClick={trashIcon}>
+                    <GoTrash className={styles.trashIcon} />
+                </button>
+            )}
+
+            {facebookIcon && (
+                <button onClick={facebookIcon}>
+                    <GrFacebook className={styles.socialMediaIcon} />
+                </button>
+            )}
+            {instagramIcon && (
+                <button onClick={instagramIcon}>
+                    <GrInstagram className={styles.socialMediaIcon} />
+                </button>
+            )}
+            {linkedinIcon && (
+                <button onClick={linkedinIcon}>
+                    <GrLinkedin className={styles.socialMediaIcon} />
+                </button>
+            )}
+            {shareIcon && (
+                <button onClick={shareIcon}>
+                    <BiSolidShareAlt className={styles.socialMediaIcon} />
+                </button>
+            )}
+            {readMoreBtn && (
+                <button className={styles.productButton} onClick={readMoreBtn}>
+                    <div className='flex items-center'>Read more</div>
+                </button>
+            )}
+            {ukFlag && (
+                <button onClick={ukFlag}>
+                    <Image
+                        src='/images/uk.png'
+                        width={50}
+                        height={50}
+                        alt='uk flag'
+                    />
                 </button>
             )}
         </div>
