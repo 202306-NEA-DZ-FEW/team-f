a2a85f2ae63aa6485df61ecc30f927e5fddc1eea
ismailbenlaredj <yako_is@yahoo.fr>
2023-10-18 12:16:25 +0100

    feat(firebase): add firestore configuration and data management functions
    configure Firestore and implement essential data management functions such as setDoc, getData, and
    updatedDoc for streamlined operations.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -16,4 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 // const analytics = getAnalytics(app);
