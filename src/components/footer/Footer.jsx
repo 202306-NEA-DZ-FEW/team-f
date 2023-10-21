b8be8b5bbe06749c728d8567f25fbd44ea0d8fb7
moh1med <mohamedch047@gmail.com>
2023-10-21 16:32:01 +0100

    feat: make it responsive with mobile version and fix margin

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Image from "next/image";
 import {
     BiLogoFacebookSquare,
     BiLogoInstagram,
@@ -8,17 +8,21 @@ import {
 
 const Footer = () => {
     return (
-        <footer className='bg-green text-white'>
-            <div className='container mx-auto flex justify-center items-center flex-wrap p-8'>
+        <footer className='bg-green text-white p-8'>
+            <div className='mx-auto flex flex-col items-center sm:flex-col lg:flex-row'>
                 {/* Logo */}
-                <div className='mx-4 mb-4 mr-8 '>
-                    <img src='./Logo/logo.png' alt='Logo' className='w-20 ' />
+                <div className='mx-2 mb-4 '>
+                    <Image
+                        src='/Logo/logo.png'
+                        alt='Logo'
+                        width={80}
+                        height={80}
+                    />
                 </div>
-
                 {/* Columns */}
-                <div className='flex flex-wrap flex-col lg:flex-row justify-center lg:justify-between w-full max-w-4xl'>
+                <div className='flex flex-col sm:flex-row lg:flex-row justify-between px-10 mx-auto w-full'>
                     {/* First Column */}
-                    <div className='flex flex-col mx-4 mb-4'>
+                    <div className='flex flex-col items-center sm:mx-2 sm:mb-4'>
                         <a
                             href='#'
                             className='text-white hover:text-black mb-1'
@@ -40,36 +44,27 @@ const Footer = () => {
                     </div>
 
                     {/* Second Column */}
-                    <div className='flex flex-col items-center mx-4 mb-4'>
+                    <div className='flex flex-col items-center sm:mx-4 sm:mb-4'>
                         <h2>Contact us</h2>
-                        <div className='flex w-full space-x-2  p-2'>
+                        <div className='flex items-center space-x-2 p-2'>
                             <a href='#' className='text-white hover:text-black'>
                                 <BiLogoFacebookSquare className='w-6 h-6' />
                             </a>
-                            <a
-                                href='#'
-                                className='text-white hover:text-black '
-                            >
+                            <a href='#' className='text-white hover:text-black'>
                                 <BiLogoInstagram className='w-6 h-6' />
                             </a>
-                            <a
-                                href='#'
-                                className='text-white hover:text-black '
-                            >
+                            <a href='#' className='text-white hover:text-black'>
                                 <BiLogoLinkedinSquare className='w-6 h-6' />
                             </a>
-                            <a
-                                href='#'
-                                className='text-white hover:text-black '
-                            >
+                            <a href='#' className='text-white hover:text-black'>
                                 <BiSolidShareAlt className='w-6 h-6' />
                             </a>
                         </div>
                     </div>
 
                     {/* Third Column */}
-                    <div className='mx-4 mb-4'>
-                        <h2>Adress</h2>
+                    <div className='mx-4 items-center flex-col flex mb-4'>
+                        <h2>Address</h2>
                         <p className='text-gray-300'>
                             123 Street, City, Country
                         </p>
