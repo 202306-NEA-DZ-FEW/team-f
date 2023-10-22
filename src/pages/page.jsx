38fc753eeda64691fd574dfd39fbbb26ebe14ce1
Sara BEGACHE <sara.begache@gmail.com>
2023-10-22 12:27:52 +0100

    fix: adding a parent link and a user member description
    BREAKING CHANGE: adding a parent link and a user member description
    
    fix #43

---
+++ src/pages/page.jsx
@@ -0,0 +1,12 @@
+import ArticleCard from "@/components/ArticleCard";
+import React from "react";
+
+const page = () => {
+    return (
+        <div>
+            <ArticleCard />
+        </div>
+    );
+};
+
+export default page;
