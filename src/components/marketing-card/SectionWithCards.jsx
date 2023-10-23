bfebd2d6c0674a1d0df12064f9e6c6577d099c10
ismailbenlaredj <yako_is@yahoo.fr>
2023-10-23 19:55:58 +0100

    refactor(marketing card): update section container component

--- src/components/marketing-card/SectionWithCards.jsx
+++ src/components/marketing-card/SectionWithCards.jsx
@@ -1,9 +1,7 @@
-import Card from "./Card";
-
-const SectionWithCards = ({ children }) => {
+const SectionWithCards = ({ children, SectionTitle }) => {
     return (
         <section className='bg-gray-100 p-8 w-5/6 mx-auto text-center my-6'>
-            <h2 className='text-2xl font-bold mb-8'>Section Title</h2>
+            <h2 className='text-2xl font-bold mb-8'>{SectionTitle}</h2>
             <div className='flex flex-wrap mx-2'>{children}</div>
         </section>
     );
