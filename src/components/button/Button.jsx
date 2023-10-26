14d636d0f6bf7a7227758bcbe8853f4c7e614449
ismailbenlaredj <yako_is@yahoo.fr>
2023-10-26 11:42:09 +0100

    feat(button component): add button with different variation
    feat #32

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,50 @@
+import { cva } from "class-variance-authority";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+const buttonVariantStyle = cva(
+    "flex items-center rounded-full gap-5 justify-center font-medium transition-colors text-base items-center",
+    {
+        variants: {
+            variant: {
+                default:
+                    "bg-green text-white hover:opacity-90 active:ring-2 active:ring-green ",
+                outlinePrimary:
+                    "bg-transparent border-2 border-green text-green hover:bg-emerald-100 active:ring-2 active:ring-green ",
+                outlineSecondary:
+                    "bg-transparent border-2 border-red text-red hover:bg-rose-100 active:ring-2 active:ring-red ",
+            },
+            size: {
+                default: "h-9 py-3 px-6 gap-3",
+                lg: "h-10 px-8 gap-4",
+                full: "w-full h-10 px-8 gap-4",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default",
+        },
+    },
+);
+const Button = ({ className, children, href, variant, size, ...props }) => {
+    if (href) {
+        return (
+            <Link
+                href={href}
+                className={cn(buttonVariantStyle({ variant, size, className }))}
+            >
+                {children}
+            </Link>
+        );
+    }
+    return (
+        <button
+            className={cn(buttonVariantStyle({ variant, size, className }))}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
