c2ed466c3f40b934dc365f1123aa50070998691e
moh1med <mohamedch047@gmail.com>
2023-10-26 17:18:59 +0100

    feat: create second hero section

---
+++ src/components/Data.js
@@ -0,0 +1,18 @@
+export const cardData = [
+    {
+        icon: <img src={"/marketing-icons/sec1.png"} alt='Icon 1' />, // Your SVG icon here
+        title: "Card 1 Title",
+        content: "Card 1 Content",
+    },
+    {
+        icon: <img src={"/marketing-icons/sec1.png"} alt='Icon 1' />, // Your SVG icon here
+        title: "Card 2 Title",
+        content: "Card 2 Content",
+    },
+    {
+        icon: <img src={"/marketing-icons/sec1.png"} alt='Icon 1' />, // Your SVG icon here
+        title: "Card 3 Title",
+        content: "Card 3 Content",
+    },
+    // Add more card data as needed
+];
