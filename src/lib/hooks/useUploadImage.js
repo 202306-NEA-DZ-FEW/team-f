8b0b435064813f59507bbfa3fea593c2b327f662
ismailbenlaredj <yako_is@yahoo.fr>
2023-10-27 20:15:30 +0100

    feat(upload images api): replace firestorage function with useUploadImage hook

---
+++ src/lib/hooks/useUploadImage.js
@@ -0,0 +1,52 @@
+import {
+    getDownloadURL,
+    ref,
+    StorageError,
+    StorageReference,
+    uploadBytesResumable,
+    UploadMetadata,
+    UploadResult,
+    UploadTaskSnapshot,
+} from "firebase/storage";
+import { useCallback, useState } from "react";
+
+import { storage } from "@/lib/firebase/firebase";
+
+const UseUploadImage = () => {
+    const [error, setError] = useState("");
+    const [uploading, setUploading] = useState(false);
+    const [snapshot, setSnapshot] = useState();
+    const [imageUrl, setImageUrl] = useState("/");
+
+    const uploadFile = useCallback(async (data, metadata = "") => {
+        const storageRef = ref(storage, `images/${data.name}`);
+        return new Promise((resolve) => {
+            setUploading(true);
+            setError(undefined);
+            const uploadTask = uploadBytesResumable(storageRef, data, metadata);
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    setSnapshot(
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+                    );
+                },
+                (error) => {
+                    setUploading(false);
+                    setError(error);
+                    resolve(undefined);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(
+                        (downloadURL) => {
+                            setImageUrl(downloadURL);
+                        },
+                    );
+                },
+            );
+        });
+    }, []);
+
+    return [uploadFile, uploading, snapshot, imageUrl, error];
+};
+export default UseUploadImage;
