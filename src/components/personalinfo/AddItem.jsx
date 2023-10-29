e3440e6a34ad57664ae0a08207f6a56a90ccbab5
Darine <tagdarinefatma@gmail.com>
2023-10-29 15:30:05 +0100

    feat(page): add SSG
    add SSG and a specific path for each id to fetch accordingly
    
    re #69

--- src/components/personalinfo/AddItem.jsx
+++ src/components/personalinfo/AddItem.jsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
 
-const AddItem = ({produt, title}) => {
+const AddItem = ({ product, title }) => {
     return (
         <div className='flex flex-col justify-center items-center m-8 gap-10'>
-            <span className='text-2xl text-zinc-700 font-bold'>My items</span>
             <div className='lg:w-9/12 w-80 h-16 flex gap-4 text-center bg-neutral-100 rounded-3xl'>
                 <Image
                     width={500}
                     height={500}
                     className='w-20 my-3 h-12 rounded-2xl'
-                    src='1.png'
+                    src='/images/uk.png'
                     alt='product'
                 />
-                <p className='text-xl my-5'>{produt} {title}</p>
+                <p className='text-xl my-5'>
+                    {product} {title}
+                </p>
                 <button></button>
                 <button></button>
             </div>
--- src/pages/account/[id].jsx
+++ src/pages/account/[id].jsx
@@ -1,64 +1,99 @@
-import { useEffect, useState } from "react";
-
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import AddItem from "@/components/personalinfo/AddItem";
 import PersonalInfo from "@/components/personalinfo/PersonalInfo";
 
-export default function Blog() {
-    const [infos, setInfos] = useState([]);
-
-    useEffect(() => {
-        async function fetchInfos() {
-            try {
-                const response = await fetch("http://localhost:4000/infos");
-                if (!response.ok) {
-                    throw new Error("Failed to fetch data");
-                }
-                const data = await response.json();
-
-                setInfos(data);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        }
-
-        fetchInfos();
-    }, []);
+export default function Information({ info }) {
+    if (!info) {
+        return (
+            <div>
+                <Navbar />
+                <Footer />
+            </div>
+        );
+    }
 
     return (
         <div>
             <Navbar />
             <ul>
-                {infos &&
-                    infos.map((info) => (
-                        <li key={info.id}>
-                            <PersonalInfo
-                                name={info.name}
-                                surname={info.surname}
-                                location={info.location}
-                                phone={info.phone}
-                                email={info.email}
-                            />
-                            <span className='flex justify-center items-center text-2xl text-zinc-700 font-bold'>
-                                My items
-                            </span>
-                            <AddItem
-                                title={info.title}
-                                product={info.product}
-                            />
-                            <AddItem
-                                title={info.title}
-                                product={info.product}
-                            />
-                            <AddItem
-                                title={info.title}
-                                product={info.product}
-                            />
-                            <Footer />
-                        </li>
-                    ))}
+                <li key={info.id}>
+                    <PersonalInfo
+                        name={info.name}
+                        surname={info.surname}
+                        location={info.location}
+                        phone={info.phone}
+                        email={info.email}
+                    />
+                    <span className='flex justify-center items-center text-2xl text-zinc-700 font-bold'>
+                        My items
+                    </span>
+                    <AddItem title={info.title} product={info.product} />
+                    <AddItem title={info.title} product={info.product} />
+                    <AddItem title={info.title} product={info.product} />
+                </li>
             </ul>
+            <Footer />
         </div>
     );
+}
+
+export async function getStaticPaths() {
+    try {
+        // Fetch the list of available info IDs from your API
+        const response = await fetch("http://localhost:4000/infos");
+        const data = await response.json();
+
+        if (!data.infos) {
+            // Handle the case where data.infos is missing or undefined
+            console.error("Data.infos is missing or undefined.");
+            return {
+                paths: [],
+                fallback: false,
+            };
+        }
+
+        // Generate paths for all available info IDs
+        const paths = data.infos.map((info) => ({
+            params: { id: info.id.toString() },
+        }));
+
+        return {
+            paths,
+            fallback: false,
+        };
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return {
+            paths: [],
+            fallback: false,
+        };
+    }
+}
+
+export async function getStaticProps({ params }) {
+    try {
+        // Fetch data for a specific info ID based on the provided 'id' parameter
+        const response = await fetch(
+            `http://localhost:4000/infos/${params.id}`,
+        );
+        const info = await response.json();
+
+        if (!info) {
+            // Handle the case where 'info' is missing or undefined
+            console.error("Info not found.");
+            return {
+                notFound: true,
+            };
+        }
+
+        return {
+            props: { info },
+        };
+    } catch (error) {
+        console.error("Error fetching info:", error);
+        return {
+            notFound: true,
+        };
+    }
 }
