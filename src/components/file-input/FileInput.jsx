9825d74f5060ce204ae57a19e86145e168a7c4c7
ismailbenlaredj <yako_is@yahoo.fr>
2023-10-30 19:06:35 +0100

    feat(add item form): create add item form & link it to firebase storage and firestore
    feat #64

--- src/components/file-input/FileInput.jsx
+++ src/components/file-input/FileInput.jsx
@@ -1,20 +1,30 @@
+import clsx from "clsx";
 import { useState } from "react";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 
 import InputImage from "./InputImage";
 
-const FileInput = ({ files, setFiles }) => {
+const FileInput = ({ files, setFiles, register, errors, clearErrors }) => {
     const [input, setInput] = useState(false);
 
     const handleChange = async (e) => {
         e.preventDefault();
+        clearErrors("image");
         setFiles(Array.from(e.target.files));
         setInput(true);
     };
 
     return (
-        <div className='relative flex justify-center items-center rounded-lg  border-dashed border-2 border-black w-full h-[20rem] my-10 cursor-pointer hover:bg-slate-100'>
+        <div
+            className={clsx(
+                "relative flex justify-center mx-auto items-center rounded-lg  border-dashed border-2  w-full lg:w-4/5 h-[20rem] my-10 cursor-pointer hover:bg-slate-100",
+                { "border-black": !errors.image, "border-red": errors.image },
+            )}
+        >
             <input
+                {...register("image", {
+                    required: "Add at least one image to continue",
+                })}
                 type='file'
                 className='w-full h-full absolute top-0 left-0 brd opacity-0 cursor-pointer'
                 onChange={handleChange}
@@ -35,10 +45,15 @@ const FileInput = ({ files, setFiles }) => {
                 <div className='flex flex-col gap-3 text-black text-center items-center'>
                     <AiOutlineCloudUpload className='text-5xl text-green ' />
                     <h3>
-                        <b>Click to upload</b> or drag and drop
+                        <b>Click to upload images</b> or drag and drop
                     </h3>
                     <p className='text-sm'>Up to 5 images</p>
                 </div>
+            )}
+            {errors.image && (
+                <span className='text-red absolute -bottom-6 left-0'>
+                    {errors.image.message}
+                </span>
             )}
         </div>
     );
--- src/pages/add-item/index.jsx
+++ src/pages/add-item/index.jsx
@@ -1,29 +1,69 @@
+import { clsx } from "clsx";
+import { serverTimestamp } from "firebase/firestore";
+import { appWithTranslation } from "next-i18next";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
+import { setDoc } from "@/lib/firebase/firestoreFunctions";
 import UseUploadImage from "@/lib/hooks/useUploadImage";
 
+import Button from "@/components/button/Button";
 import FileInput from "@/components/file-input/FileInput";
 
-function AddItemPage() {
-    const [files, setFiles] = useState([]);
-    const [uploadFile, uploading, snapshot, imageUrl, error] = UseUploadImage();
+import { useAuth } from "@/context/AuthContext";
 
-    const handleUpload = () => {
-        files.map((file) => {
-            uploadFile(file);
+function AddItemPage() {
+    const { currentUser } = useAuth();
+    const [files, setFiles] = useState([]);
+    const [uploadFile] = UseUploadImage();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+        reset,
+        clearErrors,
+    } = useForm();
+
+    const submitItem = async (data) => {
+        const uploadPromises = files.map(async (file) => {
+            return await uploadFile(file);
         });
+
+        const downloadUrls = await Promise.all(uploadPromises);
+
+        const itemData = {
+            title: data.itemName,
+            categories: data.category,
+            description: data.description,
+            location: data.location,
+            imagesList: downloadUrls,
+            createdAt: serverTimestamp(),
+            updatedAt: serverTimestamp(),
+            userId: currentUser.uid,
+            userData: {
+                name: currentUser.displayName,
+                profileImage: currentUser.photoURL,
+            },
+        };
+        await setDoc(itemData, "items");
+        reset();
     };
-    console.log(uploading);
-    console.log(imageUrl);
     return (
-        <div>
+        <div className='mb-28'>
             <h1 className='text-center text-3xl font-semibold mt-16 mb-10 text-black'>
                 Add new Item
             </h1>
             <form className='flex flex-col w-[80%] lg:w-[60%] mx-auto text-black'>
                 <div className='mb-4'>
+                    <FileInput
+                        files={files}
+                        setFiles={setFiles}
+                        register={register}
+                        errors={errors}
+                        clearErrors={clearErrors}
+                    />
                     <label
-                        for='item-name'
+                        htmlFor='item-name'
                         className='block text-sm font-medium text-slate-700'
                     >
                         Item Name
@@ -31,66 +71,115 @@ function AddItemPage() {
                     <input
                         type='text'
                         id='item-name'
-                        name='item-name'
-                        className='mt-1 p-2 block w-full border border-slate-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green'
+                        {...register("itemName", {
+                            // required: false,
+                            required: "item name is Required",
+                            maxLength: 50,
+                            minLength: {
+                                value: 2,
+                                message:
+                                    "item name must be at least 2 characters",
+                            },
+                        })}
+                        placeholder='Ex: Math Book'
+                        className={clsx(
+                            "mt-1 p-2 block w-full border rounded-md focus:outline-none focus:ring-2 focus:ring-green",
+                            {
+                                "border-red": errors.itemName,
+                                "border-slate-300": !errors.itemName,
+                            },
+                        )}
                     />
+                    {errors.itemName && (
+                        <span className='text-red'>
+                            {errors.itemName.message}
+                        </span>
+                    )}
                 </div>
 
                 <div className='mb-4'>
                     <label
-                        for='category'
+                        htmlFor='category'
                         className='block text-sm font-medium text-slate-700'
                     >
                         Category
                     </label>
                     <select
+                        {...register("category", {
+                            required: "Select a category",
+                        })}
                         id='category'
-                        name='category'
                         className='mt-1 p-2 block w-full border border-slate-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green'
                     >
-                        <option value='category1'>Category 1</option>
+                        <option value='' defaultValue disabled>
+                            Select category{" "}
+                        </option>
                         <option value='category2'>Category 2</option>
                         <option value='category3'>Category 3</option>
                     </select>
+                    {errors.category && (
+                        <span className='text-red'>
+                            {errors.category.message}
+                        </span>
+                    )}
                 </div>
 
                 <div className='mb-4'>
                     <label
-                        for='location'
+                        htmlFor='location'
                         className='block text-sm font-medium text-slate-700'
                     >
                         Location
                     </label>
                     <input
+                        {...register("location", {
+                            // required: false,
+                            required: "Add a location ",
+                            maxLength: 50,
+                            minLength: {
+                                value: 4,
+                                message:
+                                    "Location must be at least 4 characters",
+                            },
+                        })}
                         type='text'
                         id='location'
-                        name='location'
                         className='mt-1 p-2 block w-full border border-slate-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green'
                     />
+                    {errors.location && (
+                        <span className='text-red'>
+                            {errors.location.message}
+                        </span>
+                    )}
                 </div>
 
                 <div className='mb-4'>
                     <label
-                        for='description'
+                        htmlFor='description'
                         className='block text-sm font-medium text-slate-700'
                     >
                         Description
                     </label>
                     <textarea
+                        {...register("description")}
                         id='description'
-                        name='description'
                         rows='3'
                         className='mt-1 p-2 block w-full border border-slate-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green'
                     ></textarea>
                 </div>
 
-                <FileInput files={files} setFiles={setFiles} />
-                <button
-                    onClick={handleUpload}
-                    className='bg-green text-white border rounded-2xl w-40 py-2.5 font-semibold'
+                <Button
+                    onClick={handleSubmit(submitItem)}
+                    variant='default'
+                    size='lg'
+                    className={clsx("mt-10 mx-auto", {
+                        "cursor-not-allowed bg-slate-300": isSubmitting,
+                        "bg-green": !isSubmitting,
+                    })}
+                    disabled={isSubmitting}
                 >
-                    upload
-                </button>
+                    upload item
+                </Button>
             </form>
         </div>
     );
