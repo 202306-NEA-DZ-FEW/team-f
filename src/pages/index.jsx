3ae21edf25da0c076914bb0bc8e32ed02c54588f
ismailbenlaredj <yako_is@yahoo.fr>
2023-10-30 19:59:04 +0100

    fix(layout): remove layout form home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     const { t } = useTranslation("common");
 
     return (
-        <Layout>
+        <>
             <p>{t("test")}</p>
             <div style={{ display: "flex", flexDirection: "row", gap: "20px" }}>
                 <Link href='/' locale='en'>
@@ -19,7 +19,7 @@ export default function HomePage() {
                     العربية
                 </Link>
             </div>
-        </Layout>
+        </>
     );
 }
 
