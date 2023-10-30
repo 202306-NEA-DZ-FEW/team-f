46959c07a01c73524e3070eca4153b98d46a246f
LaidBenglia <73167661+LaidBengli@users.noreply.github.com>
2023-10-30 02:16:31 -0700

    feat: create signe up page

---
+++ src/pages/signeup/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SignUpForm from "@/components/signup/SignUpForm";
+const index = () => {
+    return (
+        <div>
+            index
+            <SignUpForm />
+        </div>
+    );
+};
+
+export default index;
