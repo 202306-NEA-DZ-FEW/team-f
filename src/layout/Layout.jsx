293299d4b9bafd6270654a5bcb84203f02787cd7
ismailbenlaredj <yako_is@yahoo.fr>
2023-11-01 19:26:06 +0100

    refactor(layout): add margin  mobile version

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/navbar/Navbar";
 export default function Layout({ children }) {
     return (
         <>
-            <div className='container mx-auto '>
+            <div className='container mx-auto px-4 lg:px-0'>
                 <Navbar />
                 {children}
             </div>
