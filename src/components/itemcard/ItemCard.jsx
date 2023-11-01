e9bb5517e83c0a25a1fb91b8d6aab230cd6ff095
ismailbenlaredj <yako_is@yahoo.fr>
2023-11-01 19:20:49 +0100

    feat(item card): rebuild item card

--- src/components/itemcard/ItemCard.jsx
+++ src/components/itemcard/ItemCard.jsx
@@ -1,30 +1,33 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import { MdLocationPin } from "react-icons/md";
 const ItemCard = ({ item }) => {
+    const { id, title, description, location, imagesList } = item;
     return (
-        <div className='bg-white shadow-md rounded-xl ml-2 mr-2 mb-2 mt-2 px-2 py-2 overflow-hidden'>
-            <div className='relative '>
-                <img
-                    src={item.image}
-                    alt={item.title}
-                    className='w-full h-48 object-cover rounded-xl'
-                />
+        <Link href={`/item/${id}`} className='cursor-pointer'>
+            <div className='flex flex-col border border-gray my-4 w-full md:w-[18rem]  h-fit shadow-lg rounded-xl p-1'>
+                <div className='w-full h-[13rem] mb-4'>
+                    <img
+                        src={imagesList[0]}
+                        alt={title}
+                        className='w-full h-full object-cover rounded-xl'
+                    />
+                </div>
+                <div className='flex flex-col justify-start items-start text-black px-2'>
+                    <h2 className='text-2xl font-bold '>
+                        {title.slice(0, 18) +
+                            `${title.length > 18 ? "..." : ""}`}
+                    </h2>
+                    <p className='w-full h-[4rem]'>
+                        {description.slice(0, 80) + "..."}
+                    </p>
+                    <div className='flex items-center my-2 self-end h-16'>
+                        <MdLocationPin className='text-red text-3xl' />
+                        <p className='text-md font-semibold '>{location}</p>
+                    </div>
+                </div>
             </div>
-            <div className='p-6'>
-                <p className='ml-0 text-2xl px-3 py-2'>
-                    <FontAwesomeIcon
-                        icon='fa-solid fa-location-dot'
-                        style={{ color: "#ff0000" }}
-                    />{" "}
-                    {item.location}
-                </p>
-
-                <h3 className='text-2xl  font-semibold text-gray-800 mb-2 '>
-                    {item.title}
-                </h3>
-                <p className='text-gray-700'>{item.description}</p>
-            </div>
-        </div>
+        </Link>
     );
 };
 
