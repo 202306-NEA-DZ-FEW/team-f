20c9b78b8aeb3aef33b2bbf12689467ab9889960
Sara BEGACHE <sara.begache@gmail.com>
2023-11-03 21:17:59 +0100

    feat: add about us page
    BREAKING CHANGE: add about us page
    
    fix #25

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,11 +1,45 @@
-function AboutPage() {
-    return <div>AboutPage</div>;
+import { withTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import teamMembers from "@/lib/teamMembers";
+
+import Container from "@/components/container";
+import DeveloperAvatar from "@/components/DeveloperAvatar";
+
+function AboutPage({ t, _nextI18Next }) {
+    const { initialLocale } = _nextI18Next;
+    const team = teamMembers(t);
+    return (
+        <>
+            <Container className='my-20'>
+                <h1 className='text-3xl font-black text-center my-10'>
+                    {t("about:teamLabel")}
+                </h1>
+                <section
+                    dir={initialLocale === "ar" ? "rtl" : "ltr"}
+                    className='flex flex-wrap gap-4 items-center justify-center w-full'
+                >
+                    {team.map((member) => (
+                        <DeveloperAvatar
+                            key={member.name}
+                            name={member.name}
+                            github={member.github}
+                            linkedin={member.linkedin}
+                            imageUrl={member.imageUrl}
+                        />
+                    ))}
+                </section>
+            </Container>
+        </>
+    );
 }
 
-export default AboutPage;
+export default withTranslation(["common", "about"])(AboutPage);
 
 export async function getStaticProps({ locale }) {
     return {
-        props: {},
+        props: {
+            ...(await serverSideTranslations(locale, ["common", "about"])),
+        },
     };
 }
