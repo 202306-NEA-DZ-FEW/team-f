841d79f72adcee574893cf31772b9ab963064cb1
moh1med <mohamedch047@gmail.com>
2023-11-07 12:28:32 +0100

    feat: fix secondherosec and herosection style

--- src/components/secondherosection/SecondHeroSec.jsx
+++ src/components/secondherosection/SecondHeroSec.jsx
@@ -4,7 +4,7 @@ const SecondHeroSection = ({ children }) => {
     return (
         <section className='relative justify-center h-full mb-10  '>
             {/* Main Content */}
-            <div className='lg:grid border border-black h-[25rem] bg-green justify-center'>
+            <div className='lg:grid border border-gray h-[25rem] bg-green justify-center'>
                 {/* Hero Image */}
                 <Image
                     className='opacity-50 h-[20rem] w-auto mt-10'
@@ -15,8 +15,8 @@ const SecondHeroSection = ({ children }) => {
                 />
 
                 {/* Card Section */}
-                <div className='absolute top-1/2 w-full'>
-                    <div className='flex flex-wrap justify-center'>
+                <div className='absolute border border-gray shadow-xl top-1/2 bg-white rounded-2xl w-full '>
+                    <div className='flex flex-row items-center justify-center'>
                         {children}
                     </div>
                 </div>
